a792f7419cc145497eaa4afc1158961739984417
anubisascends <anubisascends@gmail.com>
2018-08-05 08:41:05 -0400

    Add project files.

---
+++ gmtobjs/IGear.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathfinderTools {
+    public interface IGear {
+        
+        string Title {
+            get;
+        }
+
+        string Description {
+            get;
+        }
+
+        float Cost {
+            get;
+        }
+
+        string Bulk {
+            get;
+        }
+
+        string Hands {
+            get;
+        }
+    }
+}
